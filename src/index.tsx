61b8770e555b80211f7b1b20b5713153969be9f4
Alexander Kachan <akach.dev@mail.ru>
2023-09-25 12:19:48 +0300

    add store

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createRoot} from 'react-dom/client';
-import AppWithReducers from "./AppWithReducers";
+import AppWithRedux from "./AppWithReducers";
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<AppWithReducers/>);
+root.render(<AppWithRedux/>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
